8ca9fd0ac1ceec3e18d8388f43b70c51b66fc24c
sarthak-gc <sarthakgc.np@gmail.com>
2025-02-15 21:07:28 +0545

    Completed Todo In TS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,5 +19,5 @@ app.use("/todo", todoRoute);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on http://localhost:", port);
 });
